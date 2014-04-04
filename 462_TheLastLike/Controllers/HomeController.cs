b5cea6c77df088847c045f413d9a2cb6f48518e6
Brian Berger <bergbria@gmail.com>
2014-04-03 19:28:10 -0600

    we can now connect to Last.fm

--- 462_TheLastLike/Controllers/HomeController.cs
+++ 462_TheLastLike/Controllers/HomeController.cs
@@ -1,6 +1,14 @@
-﻿using System;
+﻿using _462_TheLastLike.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,6 +16,9 @@ namespace _462_TheLastLike.Controllers
 {
     public class HomeController : Controller
     {
+        private const string LastFMAPIKey = "b06a27c2149690731f77c269f1810928";
+        private const string LastFMAPISecret = "98b1814c8e12a1a5a651994c1a16401b";
+
         public ActionResult Index()
         {
             return View();
@@ -23,6 +34,39 @@ namespace _462_TheLastLike.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult LastFMCallback()
+        {
+            string token = HttpUtility.ParseQueryString(Request.Url.Query).Get("token");
+            var db = new ApplicationDbContext();
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            var user = userManager.FindById(User.Identity.GetUserId());
+
+            string apiSigPreHash = "api_key" + LastFMAPIKey + "methodauth.getSessiontoken" + token + LastFMAPISecret;
+            string apiSig = Lastfm.Utilities.md5(apiSigPreHash);
+
+            string queryString =
+                "&token=" + token +
+                "&api_key=" + LastFMAPIKey +
+                "&api_sig=" + apiSig +
+                "&format=json";
+
+            string baseURL = "http://ws.audioscrobbler.com/2.0/?method=auth.getSession";
+            string url = baseURL + queryString;
+            
+            WebRequest request = WebRequest.Create(url);
+
+            var response = request.GetResponse();
+            string responseData = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            dynamic foo = JObject.Parse(responseData);
+            string sessionKey = foo.session.key;
+
+            user.LastFMSessionKey = token;
+            db.SaveChanges();
 
             return View();
         }
--- 462_TheLastLike/Models/IdentityModels.cs
+++ 462_TheLastLike/Models/IdentityModels.cs
@@ -5,7 +5,8 @@ namespace _462_TheLastLike.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        public string LastFMToken { get; set; }
+        public string LastFMSessionKey { get; set; }
+
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
